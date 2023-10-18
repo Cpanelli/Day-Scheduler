f5fccfbc9dceb8b85d0cbb82b51e69ebbc244611
Cpanelli <133393733+Cpanelli@users.noreply.github.com>
2023-10-18 12:49:21 -0400

    made changes for everything to work

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
-$(function() {
+$(function () {
   var saveButton = $(".saveBtn");
-  saveButton.on("click", function (){
+  saveButton.on("click", function () {
     var key = $(this).parent().attr("id");
-    var description = (this).siblings("textarea").val();
+    var description = $(this).siblings("textarea").val();
     localStorage.setItem(key, description);
-    key.children("textarea").val(localStorage.getItem(key));
-  })
+    $(this).siblings("textarea").val(localStorage.getItem(key));
+  });
 
+  var day = dayjs().day();
+  var weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  console.log(day);
+  var now = dayjs().format("MM/DD/YYYY");
+  var hour = dayjs().format("HH");
+  $("#currentDay").text(weekday[day] + " " + now);
 
-var day = dayjs().day();
-var weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-console.log(day);
-var now = dayjs().format('MM/DD/YYYY');
-var hour = dayjs().format('HH');
-$("#currentDay").text(weekday[day] + " " + now);
-
-for ( i = 9; i < 20; i++) {
-if (i < hour) {
-  $("#hour-" + i).attr("class", "past row time-block");
-  } else if (i === hour) {
-    $("#hour-" + i).attr("class", "present row time-block");
-  } else {
-    $("#hour-" + i).attr("class", "future row time-block");
-  } }
+  for (i = 9; i < 20; i++) {
+    if (i < dayjs().hour()) {
+      $("#hour-" + i).attr("class", "past row time-block");
+    } else if (i === dayjs().hour()) {
+      $("#hour-" + i).attr("class", "present row time-block");
+    } else {
+      $("#hour-" + i).attr("class", "future row time-block");
+    }
+  }
 
   for (i = 9; i < 20; i++) {
     var div = $("#hour-" + i);
     div.children("textarea").val(localStorage.getItem("hour-" + i));
   }
-})
-
-   
-   
+});
